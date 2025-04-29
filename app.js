4af6768f35dea888806588f7cf1b03939aeef366
abdulshakur <abdulshakurmlawal8@gmail.com>
2025-04-29 23:52:47 +0100

    Your commit message

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ if (!dbUrl || !SECRET_KEY) {
 
 // Middleware
 app.use(cors({
-  origin: 'https://halal-1.onrender.com',
+  origin: 'https://halalmatch.vercel.app',
   credentials: true,
 }));
 app.use(cookieParser());
