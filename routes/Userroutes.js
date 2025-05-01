04ae237414c8df31b1fadd06a50b250f2d1561e6
abdulshakur <abdulshakurmlawal8@gmail.com>
2025-05-01 23:00:39 +0100

    Fix path issue

--- routes/Userroutes.js
+++ routes/Userroutes.js
@@ -3,17 +3,15 @@ const router = express.Router();
 const upload = require("../middleware/Fileuploads")
 const authenticateToken = require('../middleware/Auth');
 
-const {CreateUser, loginUser, getsingleUser, getAllUsers, updateUser, deleteUser, requestPasswordReset, resetPassword, acknowledgeConsent} = require("../controllers/UserController")
+const {CreateUser, loginUser, getsingleUser, getAllUsers, updateUser, deleteUser, requestPasswordReset, resetPassword,} = require("../controllers/UserController")
 
 router.post("/register-User", upload.single('avatar'), CreateUser);
 router.post("/user-login", loginUser);
-router.post("/consent", acknowledgeConsent);
 router.put('/update-user/:id', authenticateToken, upload.single('avatar'), updateUser);
 router.post("/request-password-reset", requestPasswordReset);
 router.post("/reset-password", resetPassword);
 router.get('/users', getAllUsers);
 router.get('/user/:id',  getsingleUser);
-  
 router.delete('/delete-user', authenticateToken, deleteUser);
 
 module.exports = router;
