8a2a2b3677b4fce51d95253dd3161edc917886e1
abdulshakur <abdulshakurmlawal8@gmail.com>
2025-06-24 13:46:35 +0100

    update

--- routes/Adminroutes.js
+++ routes/Adminroutes.js
@@ -12,12 +12,12 @@ const {
     deleteAdmin,
     verifyAdmin,
 } = require("../controllers/AdminController");
-const { verifyUser } = require("../controllers/UserController");
+const { manualactvateuser } = require("../controllers/UserController");
 
 
 router.post("/register-Admin", upload.single('avatar'), CreateAdmin);
 router.post("/Admin-login", loginAdmin);
-router.put('/users/:userId/verify', Protected, verifyUser);
+router.put('/users/:userId/manualverify', Protected, manualactvateuser);
 router.put('/admins/:userId/verify', Protected, verifyAdmin);
 router.put('/update-Admin/:id',     Protected, upload.single('avatar'), updateAdmin);
 router.get('/all-Admins', Protected, getAllAdmins);
