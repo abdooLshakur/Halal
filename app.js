ff5b43c3de20c992dca847c3763523590aaa894b
abdulshakur <abdulshakurmlawal8@gmail.com>
2025-07-08 21:57:38 +0100

    update

--- app.js
+++ app.js
@@ -58,7 +58,6 @@ app.options('*', cors({
 app.use(cookieParser());
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true }));
-app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 // Database connection
 mongoose.connect(dbUrl)
