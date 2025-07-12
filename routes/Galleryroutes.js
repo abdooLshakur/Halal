6a8740b1865de3a944a30eb6d2bdbc333c266c7f
abdulshakur <abdulshakurmlawal8@gmail.com>
2025-07-12 23:20:50 +0100

    update

--- routes/Galleryroutes.js
+++ routes/Galleryroutes.js
@@ -4,9 +4,9 @@ const upload = require("../middleware/Fileuploads")
 const Protected = require('../middleware/Aminauth');
 const { deleteGallery, updateGallery, getAllGallery, CreateGallery, } = require("../controllers/Gallerycontroller");
 
-router.post("/create-Gallery", Protected, upload.array("Gallery_img", 10) , CreateGallery);
+router.post("/create-Gallery", Protected, upload.array("Gallery_img", 20) , CreateGallery);
 router.get("/Gallerys", getAllGallery);
-router.put("/update-Gallery/:id", Protected, upload.array("Gallery_img", 10) , updateGallery);
+router.put("/update-Gallery/:id", Protected, upload.array("Gallery_img", 20) , updateGallery);
 router.delete("/delete-Gallery/:id", Protected, deleteGallery);
 
 module.exports = router;
